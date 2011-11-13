dec68542ecaa8a5f4a2a75348b72d7a9bb8d0a29
Dennis Burton <dennis.burton@gmail.com>
2011-11-12 19:20:20 -0500

    Added permissions screen for admins

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,14 @@ Ticketee::Application.routes.draw do
 
   namespace :admin do
     root :to => "base#index"
-    resources :users
+    resources :users do
+      resources :permissions
+    end
   end
+
+  put '/admin/users/:user_id/permissions',
+    :to => 'admin/permissions#update',
+    :as => :update_user_permissions
   
   devise_for :users, :controllers => { :registrations => "registrations" }
 
--- features/step_definitions/permission_steps.rb
+++ features/step_definitions/permission_steps.rb
@@ -1,12 +1,15 @@
-#And "user@ticketee.com" can edit tickets in the "TextMate 2"" project
 permission_step =/^"([^"]*)" can ([^"]*?) ([o|i]n)?\s?the "([^"]*)" project$/
 
 Given permission_step do |user, permission, on, project|
   create_permission(user, find_project(project), permission)
 end
-#Given permission_step do |user, permission, on, project|
-#  create_permission(user, find_project(project), permission)
-#end
+
+When /^I check "([^"]*)" for "([^"]*)"$/ do |permission, name|
+  project = Project.find_by_name!(name)
+  permission = permission.downcase.gsub(" ", "_")
+  field_id = "permissions_#{project.id}_#{permission}"
+  steps(%Q{When I check "#{field_id}"})
+end
 
 def create_permission(email, object, action)
   Permission.create!( :user => User.find_by_email(email),
